d8b89940be6199d607a3e6fd0fb28f05ea7c0baf
NorthFer <102831823+NorthFer@users.noreply.github.com>
2022-04-01 14:21:48 -0700

    d

--- Ferdammt.py
+++ Ferdammt.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-df=pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 #st.title("Mi primer app")
 #st.button("FOOORNAIT")
 #st.button("LABABAYI")
 st.write(df)
-st.map
+st.map(df)
