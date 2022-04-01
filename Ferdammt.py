bbb214fcd1a53e279bb19e0681b8428be1507fce
NorthFer <102831823+NorthFer@users.noreply.github.com>
2022-04-01 14:19:06 -0700

    s

--- Ferdammt.py
+++ Ferdammt.py
@@ -1,4 +1,7 @@
 import streamlit as st
-st.title("Mi primer app")
-st.button("FOOORNAIT")
-st.button("LABABAYI")
+import pandas as pd
+
+df=pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+#st.title("Mi primer app")
+#st.button("FOOORNAIT")
+#st.button("LABABAYI")
